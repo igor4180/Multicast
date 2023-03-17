f2a7066bc81ef03bca0d6bbad9067e03008062fb
--- <---.mainry>
2023-03-17 20:06:50 +0300

    Добавьте файлы проекта.

---
+++ Multicast/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multicast
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			Thread receiverThread = new Thread(new ThreadStart(Receiver));
+			receiverThread.IsBackground = true;
+			receiverThread.Start();
+		}
+		delegate void receiverDelegate(string text);
+		void ReciverDel(string text)
+		{
+			richTextBox1.Text = text;
+		}
+		void Receiver()
+		{
+			while (true)
+			{
+				Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				IPEndPoint point = new IPEndPoint(IPAddress.Any, 4567);
+				socket.Bind(point);
+				IPAddress address = IPAddress.Parse("234.5.5.5");
+				socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(address, IPAddress.Any));
+				byte[] buffer = new byte[1024];
+				socket.Receive(buffer);
+				richTextBox1.Invoke(new receiverDelegate(ReciverDel), Encoding.UTF8.GetString(buffer));
+				socket.Close();
+
+			}
+		}
+	}
+}
---
+++ MulticastServer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MulticastServer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			sender.IsBackground = true;
+			sender.Start();
+		}
+
+		Thread sender = new Thread(new ThreadStart(Sender));
+		static string message = "";
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+		static void Sender()
+		{
+			while (true)
+			{
+				Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+				IPAddress dest = IPAddress.Parse("234.5.5.5");
+				socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(dest));
+				IPEndPoint point = new IPEndPoint(dest, 4567);
+				socket.Connect(point);
+				socket.Send(Encoding.UTF8.GetBytes(message));
+				socket.Close();
+				Thread.Sleep(1000);
+			}
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+			message = richTextBox1.Text;
+		}
+	}
+		
+
+}
